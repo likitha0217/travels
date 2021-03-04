f3ba43eb8fb12a573ebb9a9338480486d44af903
Likitha <lickys44@gmail.com>
2021-03-04 15:34:11 +0530

    First commit

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,40 +1,35 @@
 import Layout from "../components/layout"
 import React from "react"
 import SEO from "../components/seo"
-
-export default function contact() {
-  return <div>
-    <Layout>
+import Img from "../assets/images/contact.jpg"
+import Contact from "../components/Contact"
+import styled from'styled-components'
+const contact = () => {
+    return (
+        <Layout>
     <SEO title="Home" />
-    <form
-    name="contact v2"
-    method="post"
-    data-netlify="true"
-    data-netlify-honeypot="bot-field"
-    onSubmit="submit"
-    >
-      <input type="hidden" name="form-name" value="contact v1"/>
-      <div hidden>
-        <input name="bot-field"/>
-      </div>
-      <div>
-        <label htmlFor="first-name">First Name</label><br/>
-        <input id="first-name" type="text" name="first-name" />
-      </div>
-      <div>
-        <label htmlFor="last-name">Last Name</label><br/>
-        <input id="last-name" type="text" name="last-name" />
-      </div>
-      <div>
-        <label htmlFor="email">Email</label><br/>
-        <input id="email" type="email" name="email" />
-      </div>
-      <div>
-        <label htmlFor="comment">Any Comments?</label><br/>
-        <textarea id="comment" name="comments"></textarea>
-      </div>
-     <button type="submit">Submit</button>
-    </form>
-    </Layout>
-  </div>
+    <ContactMainContainer>
+       <ContactImage>
+         <img src={Img} alt=""/>
+       </ContactImage>
+       <Contact/>
+
+    </ContactMainContainer>
+
+     
+   
+   
+    
+  </Layout>
+    )
 }
+
+export default contact
+const ContactMainContainer =styled.div``
+
+const ContactImage = styled.div`
+img {
+    width: 100%;
+    margin-top: -81px;
+}
+`
